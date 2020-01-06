0563351c8c1be71bc4b7f6a1afa231239f406d04
Tristan Freeman <tfreeman@playmakerhealth.com>
2020-01-06 09:09:43 -0500

    Initial commit. Setup packages, navigation, and classes

---
+++ Forcast_MVVM/app/src/main/java/com/tristanfreeman/forcastmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.tristanfreeman.forcastmvvm.ui.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+}
---
+++ Forcast_MVVM/app/src/main/java/com/tristanfreeman/forcastmvvm/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.tristanfreeman.forcastmvvm.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
